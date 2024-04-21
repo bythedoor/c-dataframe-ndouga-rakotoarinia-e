d614984c749450988100471662c4e87126762935
jenniferrkt <150791372+jenniferrkt@users.noreply.github.com>
2024-04-21 13:34:34 +0200

    Update dataframe.h

--- dataframe.h
+++ dataframe.h
@@ -17,4 +17,21 @@ DATAFRAME* create_dataframe(char title);
 
 int insert_value_dataframe(DATAFRAME *dtf, int value);
 
+void printDataFrame(DATAFRAME *dtf);
+
+void limit_lig_DataFrame(DATAFRAME *dtf, int limit_lig);
+
+void limit_col_DataFrame(DATAFRAME *dtf, int limit_col);
+
+void print_nbr_lig(DATAFRAME *dtf);
+
+void print_nbr_col(DATAFRAME *dtf);
+
+int cell_equal_df(DATAFRAME *dtf, int x);
+
+int cell_superior_df(DATAFRAME *dtf, int x);
+
+int cell_inferior_df(DATAFRAME *dtf, int x);
+
+
 #endif //C_DATAFRAME_NDOUGA_RAKOTOARINIA_E_DATAFRAME_H
