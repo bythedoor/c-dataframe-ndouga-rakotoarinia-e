389a97f0ea64bc810afe67fd8c61734789f25cd7
jenniferrkt <rakotoariniajennifer@gmail.com>
2024-05-17 11:48:25 +0200

    update supprimer_colonne

--- cmake-build-debug/CMakeFiles/c_dataframe_ndouga_rakotoarinia_e.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for c_dataframe_ndouga_rakotoarinia_e.
-# This may be replaced when dependencies are built.
--- liste.h
+++ liste.h
@@ -11,7 +11,10 @@ enum enum_type
 {
     NULLVAL = 1 , UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
 };
-typedef enum enum_type ENUM_TYPE;
+
+typedef enum enum_type ENUM_TYPE; {
+        char *nom;
+};
 
 union column_type{
     unsigned int uint_value;
