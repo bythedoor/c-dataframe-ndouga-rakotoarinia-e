0218526d2cb50c60cc105d363dad3221cd0ab8bf
jenniferrkt <150791372+jenniferrkt@users.noreply.github.com>
2024-05-20 17:47:43 +0200

    ajout fonction cdataframe

--- cdataframe.c
+++ cdataframe.c
@@ -58,3 +58,190 @@ void print_cdataframe(CDATAFRAME **cdf) {
         }
     }
 }
+
+void delete_column_cdataframe(CDATAFRAME *cdf, char *col_name) {
+    // Vérifier si la liste est vide
+    if (cdf->head == NULL || cdf->head->next == NULL) {
+        printf("Le dataframe est vide ou ne contient qu'une seule colonne.\n");
+        return;
+    }
+
+    // Supprimer la première colonne si elle correspond au titre donné
+    COLUMN *first_col = cdf->head->next->data;
+    if (strcmp(first_col->title, col_name) == 0) {
+        LNODE *first_col_node = cdf->head->next;
+        delete_column(first_col);
+        lst_delete_lnode(cdf, first_col_node);
+        printf("La colonne '%s' a ete supprimee avec succes.\n", col_name);
+        return;
+    }
+
+    // Trouver le nœud de la colonne à supprimer
+    LNODE *prev_node = cdf->head;
+    LNODE *current_node = prev_node->next;
+    while (current_node != NULL) {
+        COLUMN *mycol = current_node->data;
+        if (strcmp(mycol->title, col_name) == 0) {
+            // Supprimer la colonne associée à ce nœud
+            delete_column(mycol);
+
+            // Supprimer le nœud de la liste chaînée
+            lst_delete_lnode(cdf, current_node);
+
+            // Si la colonne supprimée est la dernière colonne, mettre à jour la queue
+            if (current_node->next == NULL) {
+                cdf->tail = prev_node;
+            }
+
+            printf("La colonne '%s' a été supprimée avec succès.\n", col_name);
+            return;
+        }
+        prev_node = current_node;
+        current_node = current_node->next;
+    }
+
+    // Si la colonne n'a pas été trouvée
+    printf("La colonne '%s' n'a pas été trouvée dans le dataframe.\n", col_name);}
+
+
+
+int get_cdataframe_cols_size(CDATAFRAME *cdf) {
+    if (cdf == NULL || cdf->head == NULL) {
+        return 0;
+    }
+
+    int count = 0;
+    LNODE *current_node = cdf->head;
+
+    while (current_node != NULL) {
+        count++;
+        current_node = current_node->next;
+    }
+
+    // Soustraire 2 pour exclure la tête et la queue de la liste chaînée
+    return count - 2;
+}
+
+void rename_column_title(CDATAFRAME *cdf, char *old_title, char *new_title) {
+    LNODE *temp = cdf->head;  // Début de la liste
+
+    // Parcourir tous les nœuds de la liste
+    while (temp != NULL) {
+        COLUMN *col = (COLUMN *)temp->data;  // Obtenir la colonne à partir du nœud
+
+        // Comparer le titre de la colonne avec l'ancien titre
+        if (strcmp(col->title, old_title) == 0) {
+            // Allouer de la mémoire pour le nouveau titre
+            col->title = (char *)malloc(strlen(new_title) + 1);
+            if (col->title == NULL) {
+                printf("Erreur lors de l'allocation de mémoire pour le nouveau titre.\n");
+                return;
+            }
+
+            // Copier le nouveau titre dans l'espace alloué
+            strcpy(col->title, new_title);
+
+            printf("Le titre de la colonne a été renommé avec succès.\n");
+            return;
+        }
+
+        temp = temp->next;  // Passer au nœud suivant dans la liste
+    }
+
+    // Si on arrive ici, cela signifie que la colonne avec l'ancien titre n'a pas été trouvée
+    printf("La colonne avec l'ancien titre n'a pas été trouvée.\n");
+}
+
+int value_in_cdataframe(CDATAFRAME *cdf, int value) {
+    if (cdf == NULL || cdf->head == NULL) {
+        return 0;
+    }
+
+    LNODE *col_node = cdf->head;
+    while (col_node != NULL) {
+        COLUMN *col = (COLUMN *)col_node->data;
+        if (nbr_occurences(col, value) > 0) {
+            return 1;
+        }
+        col_node = col_node->next;
+    }
+    return 0;
+}
+
+int count_cdataframe_rows(CDATAFRAME *cdf) {
+    if (cdf == NULL || cdf->head == NULL) {
+        return 0;
+    }
+
+    int row_count = 0;
+    LNODE *col_node = cdf->head;
+    COLUMN *first_col = col_node->data;
+
+    // Nombre de lignes est égal à la taille de la première colonne
+    row_count = first_col->t_log;
+
+    return row_count;
+}
+
+int count_cdataframe_col(CDATAFRAME *cdf) {
+    int count = 0;
+    LNODE *current_node = cdf->head;
+
+    // Parcours de la liste chaînée de colonnes
+    while (current_node != NULL) {
+        count++;
+        current_node = current_node->next;
+    }
+
+    return count;}
+
+
+int values_equal_cdataframe(CDATAFRAME *cdf, int x) {
+    int count = 0;
+    LNODE *col_node = cdf->head;
+
+    // Parcours de la liste chaînée de colonnes
+    while (col_node != NULL) {
+        COLUMN *col = (COLUMN *)col_node->data;
+        if (col != NULL) {
+            count += values_equal(col, x); // Appel de la fonction values_equal pour chaque colonne
+        }
+        col_node = col_node->next;
+    }
+
+    return count;
+}
+
+int values_superior_cdataframe(CDATAFRAME *cdf, int x) {
+
+    int count = 0;
+    LNODE *col_node = cdf->head;
+
+    // Parcours de la liste chaînée de colonnes
+    while (col_node != NULL) {
+        COLUMN *col = (COLUMN *)col_node->data;
+        if (col != NULL) {
+            count += values_superior(col, x); // Appel de la fonction values_superior pour chaque colonne
+        }
+        col_node = col_node->next;
+    }
+
+    return count;
+}
+
+int values_inferior_cdataframe(CDATAFRAME *cdf, int x) {
+
+    int count = 0;
+    LNODE *col_node = cdf->head;
+
+    // Parcours de la liste chaînée de colonnes
+    while (col_node != NULL) {
+        COLUMN *col = (COLUMN *)col_node->data;
+        if (col != NULL) {
+            count += values_inferior(col, x); // Appel de la fonction values_superior pour chaque colonne
+        }
+        col_node = col_node->next;
+    }
+
+    return count;
+}
